30911218e65ab456e7abf235f15443a7262aa898
HalloweenJack2606 <mcoloniab18@gmail.com>
2024-03-06 16:22:21 -0500

    url

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-let baseURL = "http://localhost:8000/api/"
-//let baseURL = "https://thr-backend.camionerosperuanos.org/api/"
+//let baseURL = "http://localhost:8000/api/"
+let baseURL = "https://thr-backend.camionerosperuanos.org/api/"
 let refresh = false;
 
 const axiosInstance = axios.create({
